c2de7492992b165ffd24be279b9cdd46ce3cf414
Vishal k <vkulkarni149@gmail.com>
2022-07-08 21:21:23 +0530

    i have added the three component class in home jsx and added style to the home page import style into home jsx file home jsx file imported into app jsx file

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import './Home.css';
 const Home = () => {
     return (
         <div className="Home">
             <div div className="profileSide">profile</div>
-            <div className="postSide">postfile</div>
+            <div className="postSide">post</div>
             <div className="rightSide">right</div>
 
         </div>
