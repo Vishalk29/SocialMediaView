38a0521bc3ede1b86fc75be9a1dabebd517107dc
Vishal k <vkulkarni149@gmail.com>
2022-07-08 21:03:26 +0530

    adding the file into src folder and adding the class inside the home and added the 3 div class

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div div className="profileSide">profile</div>
+            <div className="postSide">postfile</div>
+            <div className="rightSide">right</div>
+
+        </div>
+    )
+}
+export default Home
