ad0df8a0ca6535686a11d1a1bc155f9dba92e2fd
Vishal k <vkulkarni149@gmail.com>
2022-07-08 22:43:04 +0530

    added the image to profile card and styling to image

--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard/ProfileCard'
 
 const Profile = () => {
     return (
         <div className='profileSilde'>
             <LogoSearch />
+            <ProfileCard />
+
         </div>
     )
 }
