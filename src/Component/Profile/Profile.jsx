7be364337570448e1217f26169c71a182cae75fd
Vishal k <vkulkarni149@gmail.com>
2022-07-08 23:29:44 +0530

    Now added the follower and follwing part also added the style in this section added the overflow clip value to get border on profile card and we have not added the style file profile so added that also

--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
 import ProfileCard from '../ProfileCard/ProfileCard'
-
+import './Profile.css'
 const Profile = () => {
     return (
         <div className='profileSilde'>
--- src/Component/ProfileCard/ProfileCard.jsx
+++ src/Component/ProfileCard/ProfileCard.jsx
@@ -13,6 +13,30 @@ const ProfileCard = () => {
                 <span> Vishal kulkarni</span>
                 <span>Software Engineer </span>
             </div>
+
+            <div className="followStatus">
+                <hr />
+
+                <div>
+                    <div className="follow">
+                        <span>2000</span>
+                        <span>Following</span>
+                    </div>
+
+                    <div className="vertical-line"></div>
+
+                    <div className="follow">
+                        <span>1</span>
+                        <span>Followers</span>
+                    </div>
+
+                </div>
+
+                <hr />
+            </div>
+            <span>
+                My Profile
+            </span>
         </div>
     )
 }
