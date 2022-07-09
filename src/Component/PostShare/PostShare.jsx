96010b565e8e31cea49b44da6c0e64a463fd7338
Vishal k <vkulkarni149@gmail.com>
2022-07-09 22:45:17 +0530

    added the functionality of the photo featching from folder by using the useref and usestate and also adding the functionality of showing the image once we selected and removing the image once we click on Cross button

--- src/Component/PostShare/PostShare.jsx
+++ src/Component/PostShare/PostShare.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import ProfileImage from '../../img/img1.png'
 import './PostShare.css'
 import { UilScenery } from '@iconscout/react-unicons'
 import { UilPlayCircle } from '@iconscout/react-unicons'
 import { UilLocationPoint } from '@iconscout/react-unicons'
 import { UilSchedule } from '@iconscout/react-unicons'
+import { UilTimes } from '@iconscout/react-unicons'
+
 const PostShare = () => {
+    const [image, setImage] = useState(null);
+    const imageRef = useRef()
+
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0];
+            setImage({
+                images: URL.createObjectURL(img)
+            })
+        }
+    }
+
+
     return (
         <div className="postShare">
             <img src={ProfileImage} alt="" />
             <div>
                 <input type="text" placeholder='Whats happening' />
                 <div className="postOptions">
-                    <div className="option" style={{ color: "var(--photo" }}>
+                    <div className="option" style={{ color: "var(--photo" }} onClick={() => imageRef.current.click()}>
                         <UilScenery />
                         Photo
                     </div>
@@ -31,7 +46,16 @@ const PostShare = () => {
                     <button className='button ps-button'>
                         Share
                     </button>
+                    <div style={{ display: "none" }}>
+                        <input type="file" name="myImage" ref={imageRef} onChange={onImageChange} />
+                    </div>
                 </div>
+                {image && (
+                    <div className="prevImage">
+                        <UilTimes onClick={() => setImage(null)} />
+                        <img src={image.images} alt="" />
+                    </div>
+                )}
             </div>
         </div>
 
